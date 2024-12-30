ed96691c5bf4d42e232b24a24a098ad038686cec
Lucia Sheel <lsheel@shaw.ca>
2024-12-29 20:17:43 -0800

    feat: implement Passport.js local authentication strategy

---
+++ config/passport.js
@@ -0,0 +1,39 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('../models/userModel');
+
+// Configure Passport to use the LocalStrategy
+passport.use(new LocalStrategy(
+  (username, password, done) => {
+    User.findOne({ username })
+      .then((user) => {
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username.' });
+        }
+
+        user.comparePassword(password)
+          .then((isMatch) => {
+            if (!isMatch) {
+              return done(null, false, { message: 'Incorrect password.' });
+            }
+            return done(null, user); // User is authenticated
+          })
+          .catch(err => done(err));
+      })
+      .catch(err => done(err));
+  }
+));
+
+// Serialize user to store user data in the session
+passport.serializeUser((user, done) => {
+  done(null, user.id); // Serialize user by their ID
+});
+
+// Deserialize user from session
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => done(null, user))
+    .catch(err => done(err));
+});
+
+module.exports = passport;
