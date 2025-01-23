323e8367c87e64cca70290b34753252cdc9af9a2
Lucia Sheel <lsheel@shaw.ca>
2025-01-23 12:25:10 -0800

    fix: simplify authentication middleware by removing unnecessary comment

--- config/authMiddleware.js
+++ config/authMiddleware.js
@@ -2,9 +2,8 @@ module.exports.isAuthenticated = (req, res, next) => {
 
   // Check if user is authenticated via Passport
   if (req.isAuthenticated()) {
-    return next(); // If authenticated, continue to the next middleware or route handler
+    return next();
   }
   
-  // If not authenticated, send a JSON error response
   return res.status(401).json({ message: 'You must be logged in to access this resource.' });
 };
