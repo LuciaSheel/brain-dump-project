32d916c584312dc56cbd266b1273d6c03effa52c
Lucia Sheel <lsheel@shaw.ca>
2025-01-23 07:25:43 -0800

    fix: remove unnecessary comments from user model

--- models/userModel.js
+++ models/userModel.js
@@ -1,12 +1,10 @@
-// models/userModel.js
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
-// Define user schema
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  notes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Note' }]  // Add a reference to the Note model
+  notes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Note' }]
 });
 
 // Presave hook to hash password before saving
@@ -32,7 +30,5 @@ userSchema.methods.comparePassword = async function (candidatePassword) {
   }
 };
 
-// Export the User model
 const User = mongoose.model('User', userSchema);
-
 module.exports = User;
