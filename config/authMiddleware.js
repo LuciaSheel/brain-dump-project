5b3f70890f71f7045371c6b97e342c70a642107b
Lucia Sheel <lsheel@shaw.ca>
2025-01-23 12:24:40 -0800

    fix: remove console logging from authentication middleware

--- config/authMiddleware.js
+++ config/authMiddleware.js
@@ -2,10 +2,9 @@ module.exports.isAuthenticated = (req, res, next) => {
 
   // Check if user is authenticated via Passport
   if (req.isAuthenticated()) {
-    console.log('User authenticated', req.user);
     return next(); // If authenticated, continue to the next middleware or route handler
   }
-  console.log('User not authenticated');
+  
   // If not authenticated, send a JSON error response
   return res.status(401).json({ message: 'You must be logged in to access this resource.' });
 };
